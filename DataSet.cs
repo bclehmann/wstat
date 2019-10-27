045a18e1e1ff7dfbfaaa75ca3ab0d5f1acb33e5e
Benjamin Lehmann <bclehmann@yahoo.ca>
2019-10-26 20:07:13 -0600

    init

---
+++ DataSet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Where1.stat
+{
+    class DataSet
+    {
+        private List<Double> set = new List<double>();
+
+        public DataSet(List<double> setList)
+        {
+            set = setList;
+            set.Sort();
+        }
+
+        public string List()
+        {
+            StringBuilder output = new StringBuilder();
+            foreach (double curr in set)
+            {
+                output.Append($"\t{curr:f9}\n");
+            }
+
+            return output.ToString();
+        }
+
+        public string Summarize()
+        {
+            int QSize = (int)Math.Floor(set.Count / 4.0);
+            double Q1 = set[QSize];
+            double Q3 = set[set.Count - QSize - 1];
+            double min = set[0];
+            double max = set[set.Count - 1];
+            double med = new DataSet(new List<Double>() { set[(int)Math.Floor((set.Count + 1) / 2.0) - 1], set[(int)Math.Ceiling((set.Count + 1) / 2.0) - 1] }).Mean();
+
+            return $"\t{min}\t{Q1}\t{med}\t{Q3}\t{max}";
+        }
+
+        public double Mean() {
+            Console.WriteLine(set[0]);
+            Console.WriteLine(set[1]);
+            double total = 0;
+            foreach (double curr in set) {
+                total += curr;
+            }
+
+            return total / set.Count;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Where1.stat
+{
+    class Program
+    {
+        private enum Operation
+        {
+            list,
+            summary
+        };
+
+        private static Dictionary<string, string> Shortcuts = new Dictionary<string, string>(){
+            { "list", "operation=list" },
+            { "summary", "operation=summary" }
+        };
+
+        private static Dictionary<string, Operation> OperationDictionary = new Dictionary<string, Operation>() {
+            { "summary", Operation.summary}
+        };
+
+        static void Main(string[] args)
+        {
+            const string set_pattern = @"set=(.+)";
+            const string operation_pattern = @"operation=(.+)";
+            RegexOptions options = RegexOptions.Multiline;
+
+            StringBuilder setRaw = new StringBuilder();
+            Operation operation = Operation.list;
+
+
+            string[] expandedArgs = new string[args.Length];
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (Shortcuts.ContainsKey(args[i].ToLower()))
+                {
+                    expandedArgs[i] = Shortcuts.GetValueOrDefault(args[i].ToLower());
+                }
+                else
+                {
+                    expandedArgs[i] = args[i];
+                }
+            }
+
+
+            foreach (string curr in expandedArgs)
+            {
+                foreach (Match m in Regex.Matches(curr, set_pattern, options))
+                {
+                    int i = 0;
+                    foreach (var g in m.Groups)
+                    {
+                        if (i != 0)
+                        {
+                            setRaw.Append(g);
+                        }
+                        i++;
+                    }
+                }
+
+                foreach (Match m in Regex.Matches(curr, operation_pattern, options))
+                {
+                    int i = 0;
+                    foreach (var g in m.Groups)
+                    {
+                        if (i != 0)
+                        {
+                            Operation tempOperation;
+                            if (OperationDictionary.TryGetValue((string)g.ToString(), out tempOperation))
+                            {
+                                operation = tempOperation;
+                            }
+                        }
+                        i++;
+                    }
+                }
+            }
+
+            if (setRaw.Length == 0)
+            {
+                setRaw.Append(Console.ReadLine());
+            }
+
+            List<string> setStringList = setRaw.ToString().Split(',').ToList();
+            DataSet set = new DataSet(setStringList.Select(s => double.Parse(s)).ToList());
+
+            switch (operation)
+            {
+                case Operation.list:
+                    Console.Write(set.List());
+                    break;
+                case Operation.summary:
+                    Console.Write(set.Summarize());
+                    break;
+            }
+
+
+            //foreach (var curr in set) {
+            //    Console.WriteLine(curr);
+            //}
+
+
+        }
+
+
+    }
+}
